5d6f4d151d90c29cbdd3f3bace68cb3fdaa1588d
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-04-04 22:14:40 +0200

    Add files via upload

--- function/unitconver.py
+++ function/unitconver.py
@@ -30,47 +30,44 @@ class OpenFile():
     
 class UnitConverter():
     def UC_convert(self, l_val, r_val, val, val_list, conv_val):
-        try:
-            if r_val == l_val:
-                return float(conv_val)
-            else:
+        if r_val == l_val:
+            return float(conv_val)
+        else:
+            pass
+        if r_val in unit_conv['basic-unit']:
+            if l_val in unit_conv['basic-unit']:
                 pass
-            if r_val in unit_conv['basic-unit']:
-                if l_val in unit_conv['basic-unit']:
-                    pass
+            else:
+                if int(unit_conv[val_list].index(l_val)) > int(unit_conv[val_list].index(r_val)):
+                    y = float(conv_val)/int(unit_conv[val][unit_conv[val_list].index(l_val)])
+                    return float(y)
                 else:
-                    if int(unit_conv[val_list].index(l_val)) > int(unit_conv[val_list].index(r_val)):
-                        y = int(conv_val)/int(unit_conv[val][unit_conv[val_list].index(l_val)])
+                    y = float(conv_val)*int(unit_conv[val][unit_conv[val_list].index(l_val)])
+                    return float(y)
+        elif r_val not in unit_conv['basic-unit']:
+            if l_val in unit_conv['basic-unit']:
+                if int(unit_conv[val_list].index(l_val)) < int(unit_conv[val_list].index(r_val)):
+                    y = float(conv_val)*int(unit_conv[val][unit_conv[val_list].index(r_val)])
+                    return float(y)
+                elif int(unit_conv[val_list].index(l_val)) > int(unit_conv[val_list].index(r_val)):
+                    y = float(conv_val)/int(unit_conv[val][unit_conv[val_list].index(r_val)])
+                    return float(y)                        
+            if l_val not in unit_conv['basic-unit']:
+                if int(unit_conv[val_list].index(l_val)) > int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
+                    x = float(conv_val)/int(unit_conv[val][unit_conv[val_list].index(l_val)])
+                    if int(unit_conv[val_list].index(r_val)) > int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
+                        y = x*int(unit_conv[val][unit_conv[val_list].index(r_val)])
                         return float(y)
-                    else:
-                        y = int(conv_val)*int(unit_conv[val][unit_conv[val_list].index(l_val)])
+                    elif int(unit_conv[val_list].index(r_val)) < int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
+                        y = x/int(unit_conv[val][unit_conv[val_list].index(r_val)])
+                        return float(y)          
+                elif int(unit_conv[val_list].index(l_val)) < int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
+                    x = float(conv_val)*int(unit_conv[val][unit_conv[val_list].index(l_val)])
+                    if int(unit_conv[val_list].index(r_val)) > int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
+                        y = x*int(unit_conv[val][unit_conv[val_list].index(r_val)])
                         return float(y)
-            elif r_val not in unit_conv['basic-unit']:
-                if l_val in unit_conv['basic-unit']:
-                    if int(unit_conv[val_list].index(l_val)) < int(unit_conv[val_list].index(r_val)):
-                        y = int(conv_val)*int(unit_conv[val][unit_conv[val_list].index(r_val)])
+                    elif int(unit_conv[val_list].index(r_val)) < int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
+                        y = x/int(unit_conv[val][unit_conv[val_list].index(r_val)])
                         return float(y)
-                    elif int(unit_conv[val_list].index(l_val)) > int(unit_conv[val_list].index(r_val)):
-                        y = int(conv_val)/int(unit_conv[val][unit_conv[val_list].index(r_val)])
-                        return float(y)                        
-                if l_val not in unit_conv['basic-unit']:
-                    if int(unit_conv[val_list].index(l_val)) > int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
-                        x = int(conv_val)/int(unit_conv[val][unit_conv[val_list].index(l_val)])
-                        if int(unit_conv[val_list].index(r_val)) > int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
-                            y = x*int(unit_conv[val][unit_conv[val_list].index(r_val)])
-                            return float(y)
-                        elif int(unit_conv[val_list].index(r_val)) < int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
-                            y = x/int(unit_conv[val][unit_conv[val_list].index(r_val)])
-                            return float(y)          
-                    elif int(unit_conv[val_list].index(l_val)) < int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
-                        x = int(conv_val)*int(unit_conv[val][unit_conv[val_list].index(l_val)])
-                        if int(unit_conv[val_list].index(r_val)) > int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
-                            y = x*int(unit_conv[val][unit_conv[val_list].index(r_val)])
-                            return float(y)
-                        elif int(unit_conv[val_list].index(r_val)) < int(unit_conv[val_list].index(unit_conv['basic-unit'][rend.unit_data_list.index(val_list)])):
-                            y = x/int(unit_conv[val][unit_conv[val_list].index(r_val)])
-                            return float(y)
-                    else:
-                        return 'Err'
-        except:
-            return 'Err'
+                else:
+                    return 'Err'
